f2415d9b3ad15af8130d59cb455114ca5943c360
alvin-kita <alvin.kita@outlook.fr>
2024-03-17 12:43:28 +0100

    Add some comments and link to documentations

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+# Documentations utilisées
+#
+# Documentations Flask
+# Accueil : https://flask.palletsprojects.com/en/3.0.x/
+# Quickstart : https://flask.palletsprojects.com/en/3.0.x/quickstart/
+# SQLite3 : https://flask.palletsprojects.com/en/3.0.x/patterns/sqlite3/
+#
+# Documentation des fonctions Python3 : https://docs.python.org/3.11/library/functions.html
+
+
 from flask import Flask, render_template, url_for, g
 from pathlib import Path
 import sqlite3
 
-
 app = Flask(__name__)
+
 
 ##############
 # Partie Web #
 ##############
 
-
+# Au lancement, envoi sur la page d'accueil
 @app.route("/", methods=["POST", "GET"])
 def index():
     css_file = url_for('static', filename='style.css')
@@ -34,14 +44,16 @@ def dashboard():
 DATABASE = 'database.db'
 
 
+# Récupération de la base de donnée
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
+    db = getattr(g, '_database', None)  # Récupération de la base de donnée
+    if db is None:  # Si elle n'existe pas : création de la connexion
         db = g._database = sqlite3.connect(DATABASE)
         db.row_factory = sqlite3.Row
     return db
 
 
+# Ferme la connexion à la base de donnée à la fin de ger_db()
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -49,6 +61,7 @@ def close_connection(exception):
         db.close()
 
 
+# Initialisation de la base de donnée "database.db" à partir de schema.sql
 def init_db():
     with app.app_context():
         db = get_db()
@@ -57,6 +70,7 @@ def init_db():
         db.commit()
 
 
+# TODO: Debug à supprimer
 def show_user():
     with app.app_context():
         db = get_db()
